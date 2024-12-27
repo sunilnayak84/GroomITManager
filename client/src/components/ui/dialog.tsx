96bdb537229c541354f28e938ece0cd92f063ac3
sunilnayak84 <sunnynayak@gmail.com>
2024-12-27 07:06:45 +0000

    Assistant checkpoint: Fix dialog portal cleanup and overlay handling
    Assistant generated file changes:
    - client/src/components/ui/dialog.tsx: Fix dialog cleanup and portal handling, Remove unused DialogOverlay component
    
    ---
    
    User prompt:
    
    make the changes to resolve the issue. I do not want to make this kworse

--- client/src/components/ui/dialog.tsx
+++ client/src/components/ui/dialog.tsx
@@ -12,27 +12,14 @@ const DialogPortal = DialogPrimitive.Portal
 
 const DialogClose = DialogPrimitive.Close
 
-const DialogOverlay = React.forwardRef<
-  React.ElementRef<typeof DialogPrimitive.Overlay>,
-  React.ComponentPropsWithoutRef<typeof DialogPrimitive.Overlay>
->(({ className, ...props }, ref) => (
-  <DialogPrimitive.Overlay
-    ref={ref}
-    className={cn(
-      "fixed inset-0 z-40 bg-black/80 data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0",
-      className
-    )}
-    {...props}
-  />
-))
-DialogOverlay.displayName = DialogPrimitive.Overlay.displayName
+
 
 const DialogContent = React.forwardRef<
   React.ElementRef<typeof DialogPrimitive.Content>,
   React.ComponentPropsWithoutRef<typeof DialogPrimitive.Content>
 >(({ className, children, ...props }, ref) => (
   <DialogPortal>
-    <DialogOverlay />
+    <DialogPrimitive.Overlay className="fixed inset-0 z-40 bg-black/80 data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0" />
     <DialogPrimitive.Content
       ref={ref}
       onOpenAutoFocus={(event) => {
